76595a944dfb5f7393f5e2784cc05506cc75fc8b
ZW <ziwei1202@gmail.com>
2020-04-03 20:03:55 +0800

    Audio and Video added
    UI not done

--- chat_function/src/js/components/controller/controllerCmp.js
+++ chat_function/src/js/components/controller/controllerCmp.js
@@ -8,128 +8,6 @@ angular.module("sample").component("rbxController", {
     
     "use strict";
 
-// ---------------------------------------------------------------------------
-
-      var listeners = [];
-
-      $scope.microphones = [];
-
-      $scope.speakers = [];
-
-      $scope.cameras = [];
-
-      $scope.isChrome = navigator.userAgent.toLowerCase().indexOf("chrome") > -1;
-      $scope.isFirefox =
-        navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
-      $scope.isOther = !($scope.isChrome || $scope.isFirefox);
-
-      $timeout(function() {
-        initialize();
-      }, 1000);
-
-      this.$onInit = function() {};
-
-      var initialize = function initialize() {
-        if ($scope.isChrome) {
-          $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_START");
-
-          // Enumerate the list of available media device
-          navigator.mediaDevices
-            .getUserMedia({ audio: true, video: true })
-            .then(function(stream) {
-              console.log("[DEMO] :: Get user media ok... Enumerate devices...");
-              stream.getTracks().forEach(function(track) {
-                track.stop();
-              });
-              navigator.mediaDevices
-                .enumerateDevices()
-                .then(gotDevices)
-                .catch(handleError);
-            })
-            .catch(function(error) {
-              console.log(
-                "[DEMO] :: Unable to have access to media devices",
-                error
-              );
-            });
-        } else if ($scope.isFirefox) {
-          $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_END");
-        } else {
-          $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_FAILED");
-        }
-
-        // Subscribe to WebRTC error
-        listeners.push(
-          document.addEventListener(
-            rainbowSDK.webRTC.RAINBOW_ONWEBRTCERRORHANDLED,
-            onWebRTCErrorHandled
-          )
-        );
-      };
-
-      var onWebRTCErrorHandled = function onWebRTCErrorHandled(event) {
-        var error = event.detail;
-        console.log("[DEMO] :: WebRTC Error", error);
-        $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_END");
-      };
-
-      var gotDevices = function gotDevices(devices) {
-        devices.forEach(function(device) {
-          switch (device.kind) {
-            case "audioinput":
-              $scope.microphones.push(device);
-              break;
-            case "audiooutput":
-              $scope.speakers.push(device);
-              break;
-            case "videoinput":
-              $scope.cameras.push(device);
-              break;
-            default:
-              console.log("Strange...", device);
-              break;
-          }
-        });
-
-        if ($scope.microphones.length === 0) {
-          $scope.microphones.push({
-            deviceId: "default",
-            groupId: "2029518264",
-            kind: "audioinput",
-            label: "No microphone"
-          });
-        }
-
-        if ($scope.speakers.length === 0) {
-          $scope.speakers.push({
-            deviceId: "default",
-            groupId: "2029518264",
-            kind: "audioinput",
-            label: "No speaker"
-          });
-        }
-
-        if ($scope.cameras.length === 0) {
-          $scope.cameras.push({
-            deviceId: "default",
-            groupId: "2029518264",
-            kind: "audioinput",
-            label: "No camera"
-          });
-        }
-        $scope.$apply(function() {
-          $scope.selectedMicrophone = $scope.microphones[0];
-          $scope.selectedSpeaker = $scope.speakers[0];
-          $scope.selectedCamera = $scope.cameras[0];
-        });
-
-        $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_END");
-      };
-
-      var handleError = function handleError(error) {
-        console.log("[DEMO] :: Devices error", error);
-        $rootScope.$broadcast("DEMO_ON_CHECK_DEVICES_END");
-      };
 
 
 // // ---------------------------------------------------------------------------
@@ -138,17 +16,19 @@ angular.module("sample").component("rbxController", {
 
     $scope.isInCommunication = false;
 
-    // $scope.isPIPDisplayed = true;
+    //-----------------------------------
 
-    // $scope.isRemoteVideoDisplayed = true;
+    $scope.isPIPDisplayed = true;
 
-    // $scope.isSpectrumDisplayed = false;
+    $scope.isRemoteVideoDisplayed = true;
 
-    // $scope.hasLocalVideo = false; //Compute local
+    $scope.isSpectrumDisplayed = false;
 
-    // $scope.hasRemoteVideo = false; // compute remote
+    $scope.hasLocalVideo = false; //Compute local
 
-    // $scope.isCheckedDisplayed = true;
+    $scope.hasRemoteVideo = false; // compute remote
+
+    $scope.isCheckedDisplayed = true;
 
     $scope.title = "Please wait...";
 
@@ -173,15 +53,15 @@ angular.module("sample").component("rbxController", {
         onWebRTCGetUserMediaErrorOccured
       );
 
-      // document.addEventListener(
-      //   rainbowSDK.webRTC.RAINBOW_ONWEBRTCTRACKCHANGED,
-      //   onWebRTCTrackChanged
-      // );
+      document.addEventListener(
+        rainbowSDK.webRTC.RAINBOW_ONWEBRTCTRACKCHANGED,
+        onWebRTCTrackChanged
+      );
 
-      // document.addEventListener(
-      //   rainbowSDK.webRTC.RAINBOW_ONCONVERSATIONCHANGED,
-      //   onConversationChanged
-      // );
+      document.addEventListener(
+        rainbowSDK.webRTC.RAINBOW_ONCONVERSATIONCHANGED,
+        onConversationChanged
+      );
 
       $rootScope.$on("DEMO_ON_CHECK_DEVICES_START", onDeviceCheckStart);
 
@@ -203,16 +83,17 @@ angular.module("sample").component("rbxController", {
       });
     };
 
+
     var onDeviceCheckFailed = function onDeviceCheckFailed() {
       $scope.title = "WARNING !";
       $scope.message =
         "This demo will not work on this browser (not compatible)";
     };
 
-    // var onConversationChanged = function onConversationChanged(event) {
-    //   var conversation = event.detail;
-    //   console.log("[DEMO] :: Conversation changed", conversation);
-    // };
+    var onConversationChanged = function onConversationChanged(event) {
+      var conversation = event.detail;
+      console.log("[DEMO] :: Conversation changed", conversation);
+    };
 
     var onConnectionStateChangeEvent = function onConnectionStateChangeEvent(
       event
@@ -227,12 +108,12 @@ angular.module("sample").component("rbxController", {
       }
     };
 
-    // var onWebRTCGetUserMediaErrorOccured = function onWebRTCGetUserMediaErrorOccured(
-    //   event
-    // ) {
-    //   var error = event.detail;
-    //   console.log("[DEMO] :: WebRTC GetUserMedia error occurs", error);
-    // };
+    var onWebRTCGetUserMediaErrorOccured = function onWebRTCGetUserMediaErrorOccured(
+      event
+    ) {
+      var error = event.detail;
+      console.log("[DEMO] :: WebRTC GetUserMedia error occurs", error);
+    };
 
     var onWebRTCCallChanged = function onWebRTCCallChanged(event, call) {
       var call = event.detail;
@@ -279,109 +160,109 @@ angular.module("sample").component("rbxController", {
       currentCall = call;
     };
 
-    // var onWebRTCTrackChanged = function onWebRTCTrackChanged(event) {
-    //   var call = event.detail;
-    //   console.log(
-    //     "[DEMO] :: WebRTC Track changed local|remote " +
-    //       call.localMedia +
-    //       "|" +
-    //       call.remoteMedia
-    //   );
-    //   // Manage remote video
-    //   if (call.remoteMedia & Call.Media.VIDEO) {
-    //     displayRemoteVideo(call);
-    //   } else {
-    //     hideRemoteVideo(call);
-    //   }
-    //   // Manage local video
-    //   if (call.localMedia & Call.Media.VIDEO) {
-    //     displayLocalVideo(call);
-    //   } else {
-    //     hideLocalVideo(call);
-    //   }
-    // };
+    var onWebRTCTrackChanged = function onWebRTCTrackChanged(event) {
+      var call = event.detail;
+      console.log(
+        "[DEMO] :: WebRTC Track changed local|remote " +
+          call.localMedia +
+          "|" +
+          call.remoteMedia
+      );
+      // Manage remote video
+      if (call.remoteMedia & Call.Media.VIDEO) {
+        displayRemoteVideo(call);
+      } else {
+        hideRemoteVideo(call);
+      }
+      // Manage local video
+      if (call.localMedia & Call.Media.VIDEO) {
+        displayLocalVideo(call);
+      } else {
+        hideLocalVideo(call);
+      }
+    };
 
-    // var answerInVideo = function answerInVideo(call) {
-    //   console.log("[DEMO] :: Answer in video");
-    //   rainbowSDK.webRTC.answerInVideo(call);
-    // };
+    var answerInVideo = function answerInVideo(call) {
+      console.log("[DEMO] :: Answer in video");
+      rainbowSDK.webRTC.answerInVideo(call);
+    };
 
     var answerInAudio = function answerInAudio(call) {
       console.log("[DEMO] :: Answer in audio");
       rainbowSDK.webRTC.answerInAudio(call);
     };
 
-    // var displayRemoteVideo = function displayRemoteVideo(call) {
-    //   console.log("[DEMO] :: Display remote video");
-    //   rainbowSDK.webRTC.showRemoteVideo(call);
-    //   $scope.hasRemoteVideo = true;
-    // };
+    var displayRemoteVideo = function displayRemoteVideo(call) {
+      console.log("[DEMO] :: Display remote video");
+      rainbowSDK.webRTC.showRemoteVideo(call);
+      $scope.hasRemoteVideo = true;
+    };
 
-    // var hideRemoteVideo = function hideRemoteVideo(call) {
-    //   console.log("[DEMO] :: Hide remote video");
-    //   rainbowSDK.webRTC.hideRemoteVideo(call);
-    //   $scope.hasRemoteVideo = false;
-    // };
+    var hideRemoteVideo = function hideRemoteVideo(call) {
+      console.log("[DEMO] :: Hide remote video");
+      rainbowSDK.webRTC.hideRemoteVideo(call);
+      $scope.hasRemoteVideo = false;
+    };
 
-    // var displayLocalVideo = function displayLocalVideo() {
-    //   console.log("[DEMO] :: Display local video");
-    //   rainbowSDK.webRTC.showLocalVideo();
-    //   $scope.hasLocalVideo = true;
-    // };
+    var displayLocalVideo = function displayLocalVideo() {
+      console.log("[DEMO] :: Display local video");
+      rainbowSDK.webRTC.showLocalVideo();
+      $scope.hasLocalVideo = true;
+    };
 
-    // var hideLocalVideo = function hideLocalVideo() {
-    //   console.log("[DEMO] :: Hide local video");
-    //   rainbowSDK.webRTC.hideLocalVideo();
-    //   $scope.hasLocalVideo = false;
-    // };
+    var hideLocalVideo = function hideLocalVideo() {
+      console.log("[DEMO] :: Hide local video");
+      rainbowSDK.webRTC.hideLocalVideo();
+      $scope.hasLocalVideo = false;
+    };
 
-    // $scope.hidePIP = function hidePIP() {
-    //   rainbowSDK.webRTC.hideLocalVideo();
-    //   $scope.isPIPDisplayed = false;
-    // };
+    $scope.hidePIP = function hidePIP() {
+      rainbowSDK.webRTC.hideLocalVideo();
+      $scope.isPIPDisplayed = false;
+    };
 
-    // $scope.showPIP = function showPIP() {
-    //   rainbowSDK.webRTC.showLocalVideo();
-    //   $scope.isPIPDisplayed = true;
-    // };
+    $scope.showPIP = function showPIP() {
+      rainbowSDK.webRTC.showLocalVideo();
+      $scope.isPIPDisplayed = true;
+    };
 
-    // $scope.hideRemote = function hideRemote() {
-    //   rainbowSDK.webRTC.hideRemoteVideo(currentCall);
-    //   $scope.isRemoteVideoDisplayed = false;
-    // };
+    $scope.hideRemote = function hideRemote() {
+      rainbowSDK.webRTC.hideRemoteVideo(currentCall);
+      $scope.isRemoteVideoDisplayed = false;
+    };
 
-    // $scope.showRemote = function showRemote() {
-    //   rainbowSDK.webRTC.showRemoteVideo(currentCall);
-    //   $scope.isRemoteVideoDisplayed = true;
-    // };
+    $scope.showRemote = function showRemote() {
+      rainbowSDK.webRTC.showRemoteVideo(currentCall);
+      $scope.isRemoteVideoDisplayed = true;
+    };
 
-    // $scope.addVideo = function addVideo() {
-    //   rainbowSDK.webRTC.addVideoToCall(currentCall);
-    // };
+    $scope.addVideo = function addVideo() {
+      rainbowSDK.webRTC.addVideoToCall(currentCall);
+    };
 
-    // $scope.removeVideo = function removeVideo() {
-    //   rainbowSDK.webRTC.removeVideoFromCall(currentCall);
-    // };
+    $scope.removeVideo = function removeVideo() {
+      rainbowSDK.webRTC.removeVideoFromCall(currentCall);
+    };
 
     $scope.release = function release() {
       rainbowSDK.webRTC.release(currentCall);
     };
 
-    // $scope.showSpectrum = function showSpectrum() {
-    //   $scope.isSpectrumDisplayed = true;
-    //   $rootScope.$broadcast(
-    //     "DEMO_ON_SPECTRUM_DISPLAY",
-    //     $scope.isSpectrumDisplayed
-    //   );
-    // };
+    $scope.showSpectrum = function showSpectrum() {
+      $scope.isSpectrumDisplayed = true;
+      $rootScope.$broadcast(
+        "DEMO_ON_SPECTRUM_DISPLAY",
+        $scope.isSpectrumDisplayed
+      );
+    };
 
-    // $scope.hideSpectrum = function hideSpectrum() {
-    //   $scope.isSpectrumDisplayed = false;
-    //   $rootScope.$broadcast(
-    //     "DEMO_ON_SPECTRUM_DISPLAY",
-    //     $scope.isSpectrumDisplayed
-    //   );
-    // };
+    $scope.hideSpectrum = function hideSpectrum() {
+      $scope.isSpectrumDisplayed = false;
+      $rootScope.$broadcast(
+        "DEMO_ON_SPECTRUM_DISPLAY",
+        $scope.isSpectrumDisplayed
+      );
+    };
 
     $scope.mute = function mute() {
       var conversationId = currentCall.conversationId;
